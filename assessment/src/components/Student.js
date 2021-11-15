0aa6597ba36f1246ad35f6e9dccd43c5ec014102
michaelporreca <88403414+michaelporreca@users.noreply.github.com>
2021-11-15 12:06:26 -0800

    completed part 4: added expandable list view of grades

--- assessment/src/components/Student.js
+++ assessment/src/components/Student.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 const Student = (props) => {
+
+    const [isHidden, setIsHidden] = useState(true)
+
+    const handleHide = () => setIsHidden(!isHidden)
+
+    const [showText, setShowText] = useState(false);
 
 
     return (
@@ -14,6 +22,26 @@ const Student = (props) => {
                 <p>Company: {props.company}</p>
                 <p>Skill: {props.skill}</p>
                 <p>Average: {props.grades.reduce((sum, curr) => sum + Number(curr), 0) / props.grades.length}%</p>
+                <div className='togglegrades'>
+                    <button onClick={() => {setShowText(!showText); handleHide()}}>
+                    {!isHidden ? (
+                            <FontAwesomeIcon icon={faMinus} size='3x' />
+                        ) : (
+                            <FontAwesomeIcon icon={faPlus} size='3x' />
+                        )}
+                    </button>
+                </div>
+                {showText && <div>
+                <br></br>
+                    <p>Test 1: {props.grades[0]}%</p>
+                    <p>Test 2: {props.grades[1]}%</p>
+                    <p>Test 3: {props.grades[2]}%</p>
+                    <p>Test 4: {props.grades[3]}%</p>
+                    <p>Test 5: {props.grades[4]}%</p>
+                    <p>Test 6: {props.grades[5]}%</p>
+                    <p>Test 7: {props.grades[6]}%</p>
+                    <p>Test 8: {props.grades[7]}%</p>
+                </div>}
             </div>
             <hr></hr>
         </div>
